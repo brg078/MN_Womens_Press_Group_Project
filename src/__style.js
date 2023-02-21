790c0deb953117bd3344157bd0f5917a2db1e17b
Brett R Gebbie <110694646+brg078@users.noreply.github.com>
2023-02-21 11:40:09 -0600

    Initialize fix-tweaks

---
+++ src/__style.js
@@ -0,0 +1,38 @@
+export const mainContentBox = {
+  bgcolor: 'grey.100',
+  padding: 1,
+  margin: 1,
+  borderRadius: 2
+}
+
+export const largeModal = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 900,
+  height: 700,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  padding: 2,
+}
+
+export const smallModal = {
+  position: 'absolute',
+  transform: 'translate(-5%, 5%)',
+  width: 180,
+  height: 64,
+  borderRadius: 2,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  padding: 2,
+}
+
+export const smallCard = {
+  width: 200,
+  height: 50,
+  margin: .5,
+  padding: .5
+}
