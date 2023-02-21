d89cc280b27e8473908fc8211f7ac7de0013e987
Brett R Gebbie <110694646+brg078@users.noreply.github.com>
2023-02-21 11:40:00 -0600

    Initialize themestory

---
+++ src/assets/AddStoryToTheme/AddStoryToTheme.jsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+
+// mui components
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import Box from '@mui/material/Box';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+
+export default function AddStoryToTheme({theme, options})  {
+    const [open, setOpen] = React.useState(false);
+    const [storyChoice, setStoryChoice] = React.useState('');
+
+    const dispatch = useDispatch();
+
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleStoryChange = (event) => {
+        const story = event.target.value;
+        setStoryChoice(story);
+        console.log('story id: ',story);
+    };
+
+    const addStoryToTheme = () => {
+        console.log(`Convert to DISPATCH. story_id:${storyChoice}, theme_id:${theme.id}`);
+        dispatch({ type: 'THEME_STORY_ADD', payload: {story_id: storyChoice, theme_id: theme.id} });
+        //();
+        setOpen(false);
+    };
+
+    return (
+        <div>
+        <IconButton onClick={() => handleClickOpen()}>
+            <ControlPointIcon />
+        </IconButton>
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+            {`Do you want to add a story to ${theme.name}`}
+            </DialogTitle>
+            <DialogTitle id="alert-dialog-title">
+            {`ID: ${theme.id}`}{`story to add:${storyChoice}`}
+            </DialogTitle>
+            {/* <DialogTitle id="alert-dialog-title">
+            {JSON.stringify(options)}
+            </DialogTitle> */}
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Search using title, subtitle, author, ...
+                </DialogContentText>
+            </DialogContent>
+            <FormControl fullWidth>
+                <InputLabel placeholder="theme-story-select">Find a Story</InputLabel>
+                <Select
+                    id="theme-story-select"
+                    value={storyChoice.id}
+                    //label=""
+                    onChange={handleStoryChange}
+                >
+                    {/* <MenuItem>Placeholder</MenuItem> */}
+                    {options.map((option) => (
+                        <MenuItem value={option.id}>
+                            {option.label}
+                        </MenuItem>
+
+                    ))}
+                    
+                </Select>
+            </FormControl>
+            <Box>
+                <Box sx={{ display: 'flex' }}>
+                    {/* <Autocomplete
+                    size='small'
+                    sx={{ width: 200 }}
+                    options={storyOptions}
+                    renderInput={(params) => <TextField {...params} size='small' label='story' />}
+                    value={contactToAdd}
+                    /> */}
+                </Box>
+            </Box>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={addStoryToTheme} autoFocus>
+                    Add Story
+                </Button>
+            </DialogActions>
+        </Dialog>
+        </div>
+    );
+}
---
+++ src/modules/makeStatusColor.js
@@ -0,0 +1,93 @@
+/* 
+  function to read the story object and get the corresponding color.
+
+  grey: past
+  red: missing at least one of: author, 
+
+*/
+import { DateTime } from 'luxon'
+
+export function makeStatusColor(story) {
+  // console.log('publication date', story)
+
+
+  const hasAuthor = !!story.contacts?.filter(contact => contact?.story_association === 'author').length
+  // console.log('author?', hasAuthor, story.contacts)
+
+  const paymentRequired = !!story.contacts?.filter(contact => contact?.invoice_amount > 0).length
+  // console.log('payment required?', paymentRequired)
+
+
+
+
+  // console.log('publication date', story.publication_date)
+
+  // const hasAuthor = true;
+  
+  // !!story.contacts?.filter(contact => contact.role === 'author').length
+  // console.log('author?', hasAuthor)
+
+  // const paymentRequired = !!story.contacts?.filter(contact=>contact.invoice_amount>0).length
+  // console.log('payment required?', paymentRequired)
+
+  const piecesToTrack = [
+    {
+      name: 'copies sent',
+      name_db: 'copies_sent',
+      status: story.copies_required,
+      complete: story.copies_sent,
+    },
+    {
+      name: 'upload photo',
+      name_db: 'photo_uploaded',
+      status: true,
+      complete: story.photo_uploaded,
+    },
+    {
+      name: 'fact-check story',
+      name_db: 'fact_check_completed',
+      status: story.fact_check_required,
+      complete: story.fact_check_completed,
+    },
+    {
+      name: 'upload graphic',
+      name_db: 'graphic_image_completed',
+      status: story.graphic_image_required,
+      complete: story.graphic_image_completed,
+    },
+    {
+      name: 'make payments',
+      name_db: 'payment_completed',
+      status: story.payment_required,
+      complete: story.payment_completed,
+    },
+    {
+      name: 'post social',
+      name_db: 'socials_completed',
+      status: story.socials_required,
+      complete: story.socials_completed,
+    },
+    {
+      name: 'underwritten',
+      name_db: 'underwriter_required',
+      status: story.underwriter_required,
+      complete: story.underwriter_completed,
+    },
+  ].filter(piece => piece.status);
+  // console.log(piecesToTrack);
+
+  const piecesReady = !piecesToTrack.filter(piece => !piece.complete).length
+  // console.log('pieces ready', piecesReady, story.title);
+  // const piecesReady = !piecesToTrack.filter(piece=>!piece.complete).length
+  // console.log('pieces ready', piecesReady);
+
+  if (story.publication_date < DateTime.now().toISO()) return { color: 'grey', notes: 'story is complete and past' }
+
+  // need to add deadlines and author to progress from red to yellow
+  if (!story.rough_draft_deadline && !story.final_draft_deadline && !story.publication_date) return { color: 'grey.100', notes: 'story has not been assigned deadlines' }
+  if (!story.rough_draft_deadline || !story.final_draft_deadline || !story.publication_date || !hasAuthor) return { color: 'red', notes: 'story is missing deadlines or an author' }
+  if (!piecesReady) return { color: 'yellow', notes: 'story has key info ready but is missing some to-do tasks' }
+  if (piecesReady) return { color: 'green', notes: 'story is ready to publish!' }
+
+  return { color: 'purple', notes: 'ummm something went wrong...' }
+}
