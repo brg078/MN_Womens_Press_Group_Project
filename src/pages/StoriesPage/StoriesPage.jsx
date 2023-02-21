b5e84fedc036bc79c8eff9b833bc1fa9369f608a
Brett R Gebbie <110694646+brg078@users.noreply.github.com>
2023-02-21 11:39:52 -0600

    Initialize feature-avatar

---
+++ src/pages/StoriesPage/StoriesPage.jsx
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from 'react';
+
+// libraries
+import { useDispatch, useSelector } from 'react-redux';
+import {DateTime} from 'luxon'
+
+// components
+import StoryListItem from '../../components/StoryListItem/StoryListItem'
+import StoryCreateEditModal from '../../components/StoryCreateEditModal/StoryCreateEditModal';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import { Box, Button, IconButton, Typography, Modal } from '@mui/material';
+import SortFilterSearch from '../../assets/SortFilterSearch/SortFilterSearch'
+
+// internal
+import { largeModal, mainContentBox } from '../../__style'
+// import { story } from '../../sampleData';
+
+export default function StoriesPage() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'GET_ALL_STORIES' })
+    dispatch({ type: 'GET_ALL_CONTACTS' })
+    dispatch({ type: 'GET_ALL_THEMES' })
+    dispatch({ type: 'GET_ALL_TAGS' })
+    dispatch({ type: 'CLEAR_TEMP_STORY'})
+  }
+    , [])
+
+  const allStories = useSelector(story => story.stories.allStories)
+  const currentStories = allStories.filter(story => DateTime.fromISO(story.publication_date) > DateTime.now())
+
+  //! temporary fix. const allStories = useSelector(store => store.stories.allStories);
+
+  // createMode: will the big story modal be in create or edit mode?
+  const [createMode, setCreateMode] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleClickPlus = () => {
+    setCreateMode(true);
+    setModalOpen(true);
+  }
+
+  const handleClose = () => {
+    setModalOpen(false)
+    dispatch({ type: 'CLEAR_TEMP_STORY' })
+  }
+
+  //* ============ SORT/FILTER/SEARCH STUFF ===============
+
+  const sortOptions = ['date', 'title'];
+  const filterOptions = ['all', 'recent'];
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortMethod, setSortMethod] = useState('date');
+  const [sortDirection, setSortDirection] = useState('ascending');
+  const [filterMethod, setFilterMethod] = useState('all')
+
+
+  const filterResults = (arr) => {
+    switch (filterMethod) {
+      case 'all':
+        return arr;
+        break;
+      // recent sets to the past three months
+      case 'recent':
+        return arr.filter(story => story.publication_date > DateTime.now().minus({ months: 3 }))
+        break;
+      default:
+        return arr
+    }
+  }
+
+  const ascDesc = (arr) => sortDirection === 'ascending' ? arr : arr.reverse()
+
+  const sortResults = (arr) => {
+
+    switch (sortMethod) {
+      case 'date':
+        return arr.sort((a, b) => {
+          if (DateTime.fromISO(a.publication_date) > DateTime.fromISO(b.publication_date)) return 1
+          if (DateTime.fromISO(a.publication_date) < DateTime.fromISO(b.publication_date)) return -1
+          else return 0
+        })
+      case 'title':
+        return arr.sort((a, b) => {
+          if (a.title > b.title) return 1
+          if (a.title < b.title) return -1
+          else return 0
+        })
+      default:
+        return arr;
+    }
+  }
+
+  const searchResults = (arr) => {
+    function getContactsString(story) {
+      return story.contacts.map(contact => contact?.name.toLowerCase()).join('')
+    }
+
+    function getTabsString(story) {
+      return story.tags.map(tag => tag?.name.toLowerCase()).join('')
+    }
+
+    return arr.filter(story => story.title.toLowerCase().includes(searchTerm) || getContactsString(story).includes(searchTerm) || getTabsString(story).includes(searchTerm) || story.theme[0]?.name.toLowerCase().includes(searchTerm))
+  }
+
+  const storyResults = ascDesc(filterResults(sortResults(searchResults(currentStories))))
+
+
+  return (
+    <Box>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography variant='h3'>Stories Page</Typography>
+        <IconButton onClick={handleClickPlus}>
+          <ControlPointIcon />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{ ...mainContentBox, height: 700, overflow: 'hidden', overflowY: 'scroll' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
+          <SortFilterSearch
+            sortOptions={sortOptions}
+            filterOptions={filterOptions}
+            sortMethod={sortMethod}
+            sortDirection={sortDirection}
+            setSortDirection={setSortDirection}
+            setSortMethod={setSortMethod}
+            filterMethod={filterMethod}
+            setFilterMethod={setFilterMethod}
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+          />
+        </Box>
+        <Box>
+          {allStories.length ? allStories.map(story => {
+            return (
+              <StoryListItem
+                key={story.title}
+                story={story}
+                createMode={createMode}
+                setCreateMode={setCreateMode}
+                setModalOpen={setModalOpen}
+              />
+            )
+          })
+          :
+          <></>
+          }
+        </Box>
+      </Box>
+      <Modal
+        open={modalOpen}
+        onClose={handleClose}>
+        <Box sx={largeModal}>
+          <StoryCreateEditModal setModalOpen={setModalOpen} createMode={createMode} setCreateMode={setCreateMode}/>
+        </Box>
+      </Modal>
+
+    </Box>
+  )
+}
---
+++ src/redux/reducers/story.reducer.js
@@ -0,0 +1,94 @@
+// import events from '../../events'
+
+import { combineReducers } from 'redux';
+import { DateTime } from 'luxon';
+
+/* 
+  - all themes in store
+  - current theme
+  - theme staging area ()
+*/
+
+const storyObject = {
+  id: '',
+  title: '',
+  subtitle: '',
+  article_text: '',
+  article_link: '',
+  notes: '',
+  photo: '',
+  type: '',
+  copies_required: 0,
+  copies_sent: false,
+  photo_required: false,
+  photo_uploaded: false,
+  fact_check_required: false,
+  fact_check_completed: false,
+  graphic_image_required: false,
+  graphic_image_completed: false,
+  payment_required: false,
+  payment_completed: false,
+  external_link: '',
+  word_count: 0,
+  date_added: '',
+  rough_draft_deadline: DateTime.now(),
+  final_draft_deadline: DateTime.now().toISO(),
+  publication_date: DateTime.now().toISO(),
+  contacts: [],
+  theme: [],
+  tags: []
+}
+
+const allStories = (state = [], action) => {
+  if (action.type === 'SET_ALL_STORIES') return action.payload
+  return state
+}
+
+const currentStory = (state = storyObject, action) => {
+  if (action.type === 'SET_CURRENT_STORY') return action.payload
+  return state
+}
+
+const blankStory = {
+  id: '',
+  title: '',
+  subtitle: '',
+  article_text: '',
+  article_link: '',
+  notes: '',
+  photo: '',
+  type: '',
+  copies_required: 0,
+  copies_sent: false,
+  photo_required: false,
+  photo_uploaded: false,
+  fact_check_required: false,
+  fact_check_completed: false,
+  graphic_image_required: false,
+  graphic_image_completed: false,
+  payment_required: false,
+  payment_completed: false,
+  external_link: '',
+  word_count: 0,
+  date_added: '',
+  rough_draft_deadline: DateTime.now(),
+  final_draft_deadline: DateTime.now().toISO(),
+  publication_date: DateTime.now().toISO(),
+  contacts: [],
+  theme: [],
+  tags: []
+}
+//! staging area, not sure if we'll use
+
+const tempStory = (state = blankStory, action) => {
+  if (action.type === 'SET_TEMP_STORY') return {...state, ...action.payload}
+  if (action.type === 'CLEAR_TEMP_STORY') return blankStory
+  return state
+}
+
+
+const storyReducer = combineReducers({
+  allStories, currentStory, tempStory
+})
+
+export default storyReducer
