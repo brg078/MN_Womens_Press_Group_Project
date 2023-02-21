2e00b6e08ed6a957f422bee91bf7a1087a39a000
Brett R Gebbie <110694646+brg078@users.noreply.github.com>
2023-02-21 11:39:41 -0600

    Initialize feature-data-templates

---
+++ server/routes/user.router.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const pool = require('../modules/pool');
+const userStrategy = require('../strategies/user.strategy');
+
+const router = express.Router();
+
+// Handles Ajax request for user information if user is authenticated
+router.get('/', rejectUnauthenticated, (req, res) => {
+  // Send back user object from the session (previously queried from the database)
+  res.send(req.user);
+});
+
+// gets all users for admin dashboard (we will filter unauthorized vs authorized on the front end so we can add/remove permissions for everyone)
+router.get('/all', rejectUnauthenticated, (req, res) => {
+  // GET request for all users goes here
+  res.sendStatus(200);
+})
+
+// Handles POST request with new user data
+// The only thing different from this and every other post we've seen
+// is that the password gets encrypted before being inserted
+router.post('/register', (req, res, next) => {
+  const username = req.body.username;
+  const password = encryptLib.encryptPassword(req.body.password);
+
+  const queryText = `INSERT INTO "user" (username, password, access)
+    VALUES ($1, $2, False) RETURNING id`;
+  pool
+    .query(queryText, [username, password])
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log('User registration failed: ', err);
+      res.sendStatus(500);
+    });
+});
+
+// Handles login form authenticate/login POST
+// userStrategy.authenticate('local') is middleware that we run on this route
+// this middleware will run our POST if successful
+// this middleware will send a 404 if not successful
+router.post('/login', userStrategy.authenticate('local'), (req, res) => {
+  res.sendStatus(200);
+});
+
+// clear all server session information about this user
+router.post('/logout', (req, res) => {
+  // Use passport's built-in method to log out the user
+  req.logout();
+  res.sendStatus(200);
+});
+
+router.put('/access', rejectUnauthenticated, (req, res) => {
+  // need to update user access based on ID (req.body.userId and req.body.access)
+  res.sendStatus(200)
+})
+
+router.delete('/', rejectUnauthenticated, (req, res) => {
+  // need route to delete User by ID (req.body === user ID)
+  res.sendStatus(200);
+})
+
+module.exports = router;
