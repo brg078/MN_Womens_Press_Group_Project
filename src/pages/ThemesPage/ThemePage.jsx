4bcb09c6314dd0eae6e1a9440a5eebdbcc776138
Brett R Gebbie <110694646+brg078@users.noreply.github.com>
2023-02-21 11:40:03 -0600

    Initialize feature-database

---
+++ src/pages/ThemesPage/ThemePage.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+
+import ThemeCalendar from '../../components/ThemeCalendar/ThemeCalendar';
+import { Typography, Box } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+
+export default function ThemesPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'GET_ALL_THEMES' });
+    dispatch({ type: 'GET_ALL_CONTACTS' });
+    dispatch({ type: 'GET_ALL_STORIES' });
+    dispatch({type:'GET_ALL_TAGS'});
+  }, [])
+
+
+
+
+  const mainContentBox = {
+    bgcolor: 'grey.100',
+    padding: 1,
+    margin: 1,
+    borderRadius: 2
+  }
+
+
+  return (
+    <div>
+      <Typography variant="h3" component="h1">
+        Themes
+      </Typography>
+      <Box sx={{bgcolor: 'grey.100',padding: 1,margin: 1,borderRadius: 2 }}>
+        <ThemeCalendar />
+      </Box>
+    </div>
+  )
+}
